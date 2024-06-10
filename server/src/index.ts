775186d615fcb655d60ecb8b7255b1ceb79ba6bf
jueun-park98 <jooeun06161@gmail.com>
2024-06-10 12:21:40 +0900

    refactor: DB 초기화 구조 변경 및 에러 발생 시 exit 처리 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,9 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import Article from './models/Article.js';
 import articleRouter from './routes/articleRouter.js';
 import { Server } from 'socket.io';
 import setupSocket from './setup/socketSetup.js';
-import { mockArticle } from './setup/mockArticle.js';
 import { setupMongoDB } from './setup/dbSetup.js';
 
 export interface CustomRequest extends Request {
@@ -22,11 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-setupMongoDB(MONGO_DB_URL).then((db) => {
-  console.log('mongoDB connected');
-  initializeDb();
-});
-
+const db = setupMongoDB(MONGO_DB_URL);
 const { server, io } = setupSocket(app);
 
 app.use(
@@ -41,17 +35,3 @@ app.use(
 server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
-const initializeDb = async () => {
-  await Article.deleteMany();
-  const articleCount = await Article.countDocuments();
-
-  if (articleCount === 0) {
-    try {
-      await Article.insertMany(mockArticle);
-      console.log('article successfully initialized');
-    } catch (error) {
-      console.error(error);
-    }
-  }
-};
