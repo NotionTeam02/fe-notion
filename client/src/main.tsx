f8778160052fb28d21e58e1b638d3100f8eb3a25
schnee98 <jooeun06161@gmail.com>
2024-06-13 20:01:42 +0900

    refactor: 스크린 스타일링 적용 위치 변경

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,20 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
-import styled from 'styled-components';
-import { FlexColumn } from './styles/themes.ts';
-
-const Screen = styled(FlexColumn)`
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-`;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Screen>
-      <RouterProvider router={router} />
-    </Screen>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -2,18 +2,39 @@ import { createBrowserRouter } from 'react-router-dom';
 import NicknameModal from '../components/modal/NicknameModal';
 import RegistrationModal from '../components/modal/RegistrationModal';
 import TeamspaceModal from '../components/modal/TeamspaceModal';
+import styled from 'styled-components';
+import { FlexColumn } from '../styles/themes';
+
+const Screen = styled(FlexColumn)`
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const router = createBrowserRouter([
   {
     path: '/login',
-    element: <NicknameModal />,
+    element: (
+      <Screen>
+        <NicknameModal />
+      </Screen>
+    ),
   },
   {
     path: '/registration',
-    element: <RegistrationModal />,
+    element: (
+      <Screen>
+        <RegistrationModal />
+      </Screen>
+    ),
   },
   {
     path: '/teamspaces',
-    element: <TeamspaceModal />,
+    element: (
+      <Screen>
+        <TeamspaceModal />
+      </Screen>
+    ),
   },
 ]);
