721553dbd2a7689aa00d4eea2d853db1241ae049
schnee98 <jooeun06161@gmail.com>
2024-06-13 19:56:27 +0900

    feat: 로그인, 회원가입, 팀스페이스 생성 api 연동

--- client/src/components/modal/TeamspacePanel.tsx
+++ client/src/components/modal/TeamspacePanel.tsx
@@ -9,17 +9,29 @@ export interface TeamspacePanelProps {
 export default function TeamspacePanel({ teamspace: { title } }: TeamspacePanelProps) {
   return (
     <Wrapper>
-      <span>{title}</span>
+      <TitleText>{title}</TitleText>
       <button>들어가기</button>
     </Wrapper>
   );
 }
 
 const Wrapper = styled(FlexColumn)`
+  box-sizing: border-box;
+  padding: 0 10px;
   width: 100px;
   height: 100px;
 
   border: 1px solid black;
   justify-content: center;
   align-items: center;
+  gap: 0.5em;
+`;
+
+const TitleText = styled.span`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  display: block;
+  text-align: center;
+  width: 100%;
 `;
