19e160ef1b8dfe711a4be2188a6a9ca32724ae20
schnee98 <jooeun06161@gmail.com>
2024-06-13 21:23:42 +0900

    feat: Article 컴포넌트 라우터 추가

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -3,7 +3,9 @@ import NicknameModal from '../components/modal/NicknameModal';
 import RegistrationModal from '../components/modal/RegistrationModal';
 import TeamspaceModal from '../components/modal/TeamspaceModal';
 import styled from 'styled-components';
-import { FlexColumn } from '../styles/themes';
+import { Flex, FlexColumn } from '../styles/themes';
+import Sidebar from '../components/sidebar/Sidebar';
+import Article from '../components/Article';
 
 const Screen = styled(FlexColumn)`
   width: 100vw;
@@ -35,6 +37,15 @@ export const router = createBrowserRouter([
       <Screen>
         <TeamspaceModal />
       </Screen>
+    ),
+  },
+  {
+    path: '/teamspace/:teamspaceId/article/:articleId',
+    element: (
+      <Flex>
+        <Sidebar />
+        <Article />
+      </Flex>
     ),
   },
 ]);
