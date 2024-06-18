a16717a36dad62304b1b579532c1c1851840447e
minjeongHEO <hmjoung9770@gmail.com>
2024-06-18 22:06:38 +0900

    feat: globalStyle 컴포넌트 적용

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
+import { GlobalStyle } from './styles/globalStyles.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GlobalStyle>
+      <RouterProvider router={router} />
+    </GlobalStyle>
   </React.StrictMode>
 );
