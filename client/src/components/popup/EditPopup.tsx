914f37c9eed31b689e2fe9d00783907ccfe5faf1
minjeongHEO <hmjoung9770@gmail.com>
2024-06-18 20:45:27 +0900

    refactor: 테마로 분리 및 삭제 컬러 적용

--- client/src/components/popup/EditPopup.tsx
+++ client/src/components/popup/EditPopup.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import themes, { FlexColumn, FlexRow } from '../../styles/themes';
-import styled from 'styled-components';
+import { PopupWrapper, PopupLineWrapper, PopupLine } from '../../styles/themes';
 import { CommentOutlined, StarFilled, DeleteOutlined, RetweetOutlined, RightOutlined } from '@ant-design/icons';
-
-const {
-  Color: { DefaultColor },
-} = themes;
 
 export default function EditPopup() {
   return (
@@ -32,7 +26,7 @@ export default function EditPopup() {
 
       <PopupLineWrapper>
         <PopupLine>
-          <div>
+          <div className="deleteTitle">
             <DeleteOutlined />
             <span className="optionTitle">삭제</span>
           </div>
@@ -54,33 +48,3 @@ export default function EditPopup() {
     </PopupWrapper>
   );
 }
-
-const PopupLineWrapper = styled.div`
-  padding: 8px;
-  border-bottom: 2px solid ${DefaultColor};
-  overflow: hidden;
-  &:last-child {
-    border-bottom: none;
-  }
-`;
-
-const PopupLine = styled(FlexRow)`
-  align-items: end;
-
-  & .optionTitle {
-    margin-left: 10px;
-  }
-  & .optionShortCutKey {
-    color: ${themes.Color.WeakColor};
-  }
-`;
-
-const PopupWrapper = styled(FlexColumn)`
-  border: 2px solid ${themes.Color.DefaultColor};
-  border-radius: 10px;
-  width: 300px;
-
-  ${PopupLineWrapper}:hover {
-    background-color: grey;
-  }
-`;
