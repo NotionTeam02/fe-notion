b50791e9b3c51a7350e89efa0fab01b74b17e123
minjeongHEO <hmjoung9770@gmail.com>
2024-06-20 12:24:59 +0900

    feat: 1뎁스 팝업 끄기

--- client/src/components/article/BlockTag.tsx
+++ client/src/components/article/BlockTag.tsx
@@ -1,5 +1,5 @@
 import { HolderOutlined, PlusOutlined } from '@ant-design/icons';
-import { FC, ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { FlexRow } from '../../styles/themes';
 import AddPopup from '../popup/AddPopup';
@@ -7,9 +7,9 @@ import EditPopup from '../popup/EditPopup';
 
 export default function BlockTag({ contentTag }: { contentTag: ReactNode }) {
   const [isShowSubPopup, setIsShowSubPopup] = useState({ edit: false, plus: false });
-
   const showEditPopup = () => setIsShowSubPopup((prev) => ({ ...prev, edit: !prev.edit }));
   const showPlusPopup = () => setIsShowSubPopup((prev) => ({ ...prev, plus: !prev.plus }));
+  const popupRef = useRef<HTMLDivElement | null>(null);
 
   const popupType: { [key: string]: () => void } = {
     edit: () => {
@@ -24,26 +24,46 @@ export default function BlockTag({ contentTag }: { contentTag: ReactNode }) {
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const target = e.currentTarget as HTMLElement;
-    const type = target.getAttribute('attr-type');
+    const type = target.getAttribute('data-attr-type');
     if (!type || !(type in popupType)) return;
     popupType[type as PopupTypeKey]();
   };
+
+  useEffect(() => {
+    const handleClickOutside = ({ target }: MouseEvent) => {
+      const popupCurrent = popupRef.current;
+      if (!popupCurrent || (popupCurrent && popupCurrent.contains(target as Node))) return;
+      setIsShowSubPopup({ edit: false, plus: false });
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
 
   return (
     <>
       <BlockWrapper>
         <Icons>
-          <IconWrapper attr-type="edit" onClick={handleClick}>
+          <IconWrapper data-attr-type="edit" onClick={handleClick}>
             <HolderOutlined />
           </IconWrapper>
-          <IconWrapper attr-type="plus" onClick={handleClick}>
+          <IconWrapper data-attr-type="plus" onClick={handleClick}>
             <PlusOutlined />
           </IconWrapper>
         </Icons>
         {contentTag}
       </BlockWrapper>
-      {isShowSubPopup.plus && <AddPopup />}
-      {isShowSubPopup.edit && <EditPopup />}
+      {isShowSubPopup.plus && (
+        <div ref={popupRef}>
+          <AddPopup />
+        </div>
+      )}
+      {isShowSubPopup.edit && (
+        <div ref={popupRef}>
+          <EditPopup />
+        </div>
+      )}
     </>
   );
 }
