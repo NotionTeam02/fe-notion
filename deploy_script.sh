6f9ea70fdbb6f4e155c76e94ac2a1ded3174570e
schnee98 <jooeun06161@gmail.com>
2024-06-26 10:16:00 +0900

    chore: 불필요한 디버깅 명령어 삭제

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,13 +1,7 @@
 export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-echo "Checking installed versions..."
-node -v
-npm -v
-npx -v
-echo "PATH: $PATH"
 rm -rf fe-notion
 git clone https://github.com/NotionTeam02/fe-notion.git
 cd fe-notion/server
 git checkout dev
 npm install
 npx tsc
-node dist/index.js
