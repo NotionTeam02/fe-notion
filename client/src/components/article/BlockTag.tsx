da0fc1dfa3aef56ca64c64ccdb302af126d3b7fe
Woody <96780693+minjeongHEO@users.noreply.github.com>
2024-06-27 17:28:03 +0900

    padding 적용 (#106)
    * refactor: 미리보기 팝업 위치 수정
    
    * style: import 절대 경로 적용
    
    * refactor: 팝업 타입 객체 수정
    
    * style: 코드 사이에 엔터 추가
    
    * refactor: 컨텐츠 반복하는 부분 함수로 분리
    
    * feat: 키이벤트 훅, 헬퍼펑션 분리
    
    * refactor: handleInput함수 분리
    
    * style: 안쓰는 코드 삭제
    
    * docs: 컴포넌트 구조 설명 추가
    
    * remove: 사용하지 않는 코드 제거
    
    * refactor: 키 이벤트 함수 분리
    
    * feat: 플러스 아이콘 클릭 시 새 블럭 생성
    
    * feat: 슬래시 눌렀을 때
    - 새 블럭 추가 안되게
    - 팝업 위치 수정
    
    * feat: 수정 팝업에서 블럭 삭제 기능
    
    * fix: props 키 이름 수정
    
    * refactor: innerHTML을 textContext로 변경
    
    * style: padding 적용
    
    * chore: gitignore 추가
    
    * chore: client ci-cd 파일 생성
    
    ---------
    
    Co-authored-by: Schnee <jooeun06161@gmail.com>

--- client/src/components/article/BlockTag.tsx
+++ client/src/components/article/BlockTag.tsx
@@ -99,7 +99,7 @@ export default function BlockTag({
       )}
       {isShowSubPopup.edit && (
         <Position ref={popupRef}>
-          <EditPopup />
+          <EditPopup {...{ contentTagRef }} />
         </Position>
       )}
     </>
